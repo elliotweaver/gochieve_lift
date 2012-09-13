ad1e7637490a0359316d9049c9799f177f023076
Elliot <elliot@oxygenproductions.com>
2012-09-12 21:02:53 -0400

    omniauth working

--- build.sbt
+++ build.sbt
@@ -7,7 +7,8 @@ organization := "net.liftweb"
 scalaVersion := "2.9.1"
 
 resolvers ++= Seq("snapshots"     at "http://oss.sonatype.org/content/repositories/snapshots",
-                "releases"        at "http://oss.sonatype.org/content/repositories/releases"
+                "releases"        at "http://oss.sonatype.org/content/repositories/releases",
+                "omniauth"        at "https://repository-liftmodules.forge.cloudbees.com/release"
                 )
 
 seq(com.github.siasia.WebPlugin.webSettings :_*)
@@ -19,8 +20,9 @@ libraryDependencies ++= {
   Seq(
     "net.liftweb"             %% "lift-webkit"         % liftVersion                  % "compile",
     "net.liftweb"             %% "lift-mongodb-record" % liftVersion                  % "compile",
-    "com.foursquare"          %% "rogue"               % "1.1.6"                      % "compile->default" intransitive(),
+    "net.liftmodules"         %% "omniauth"            % "2.4-0.5",
     "net.liftmodules"         %% "lift-jquery-module"  % (liftVersion + "-1.0"),
+    "com.foursquare"          %% "rogue"               % "1.1.6"                      % "compile->default" intransitive(),
     "org.eclipse.jetty"       %  "jetty-webapp"        % "8.1.7.v20120910"            % "container",
     "org.eclipse.jetty.orbit" %  "javax.servlet"       % "3.0.0.v201112011016"        % "container" artifacts Artifact("javax.servlet", "jar", "jar"),
     "ch.qos.logback"          %  "logback-classic"     % "1.0.6",
--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -11,24 +11,34 @@ import Loc._
 import net.liftmodules.JQueryModule
 import net.liftweb.http.js.jquery._
 
+import code.db.{MongoConfig}
+import code.model.{User}
+import code.lib.{FacebookProviderCustom}
+
+//import omniauth.lib.{TwitterProvider, FacebookProvider}
+import omniauth.lib._
+import omniauth.Omniauth;
 
 /**
  * A class that's instantiated early and run.  It allows the application
  * to modify lift's environment
  */
-class Boot {
+class Boot extends Loggable {
   def boot {
+    
     // where to search snippet
     LiftRules.addToPackages("code")
 
     // Build SiteMap
     val entries = List(
-      Menu.i("Home") / "index", // the simple way to declare a menu
-
-      // more complex because this menu allows anything in the
-      // /static path to be visible
-      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
-	       "Static Content")))
+      Menu.i("Home") / "index" // the simple way to declare a menu
+    ) ::: Omniauth.sitemap ::: User.sitemap
+    
+    //println(Omniauth.sitemap)
+    //println(entries)
+    
+	//Set sitemap
+    //LiftRules.setSiteMapFunc(() => User.sitemapMutator(SiteMap(entries:_*)))
 
     // set the sitemap.  Note if you don't want access control for
     // each page, just comment this line out.
@@ -46,13 +56,21 @@ class Boot {
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
 
     // Use HTML5 for rendering
-    LiftRules.htmlProperties.default.set((r: Req) =>
-      new Html5Properties(r.userAgent))
+    LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))
 
     //Init the jQuery module, see http://liftweb.net/jquery for more information.
     LiftRules.jsArtifacts = JQueryArtifacts
-    JQueryModule.InitParam.JQuery=JQueryModule.JQuery172
+    JQueryModule.InitParam.JQuery = JQueryModule.JQuery172
     JQueryModule.init()
+    
+    //Mongo config init
+    MongoConfig.init
+      
+    //Omni Facebook init
+	Omniauth.initWithProviders(List(new FacebookProvider("279073072192775", "52b1db374376abca115dd031cc2adcab")))
+	
+	// What is the function to test if a user is logged in?
+    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
 
   }
 }
